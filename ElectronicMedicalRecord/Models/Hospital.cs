f1f767d742332147470e2d844cdad86c1f3f339d
nemanjaletic <nemanja.letic@gmail.com>
2019-07-16 10:02:08 +0200

    Added properties in Hospital

--- ElectronicMedicalRecord/Models/Hospital.cs
+++ ElectronicMedicalRecord/Models/Hospital.cs
@@ -7,5 +7,10 @@ namespace ElectronicMedicalRecord.Models
 {
     public class Hospital
     {
+        public int HospitalID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
     }
 }
