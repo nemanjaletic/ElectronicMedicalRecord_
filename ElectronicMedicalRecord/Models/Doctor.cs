95eaf708ae40ba1b47092542d9116f77eada0ed9
nemanjaletic <nemanja.letic@gmail.com>
2019-07-16 10:37:13 +0200

    Added Doctor class Added Patient class

---
+++ ElectronicMedicalRecord/Models/Doctor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ElectronicMedicalRecord.Models
+{
+    public class Employed
+    {
+        public int EmployedID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string UCIN { get; set; }
+        public string LicenseNumber { get; set; }
+        public DateTime DateOfEmployment { get; set; }
+        public string SecurityQuestion { get; set; }
+        public string SecurityAnswer { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int HospitalID { get; set; }
+    }
+}
---
+++ ElectronicMedicalRecord/Models/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ElectronicMedicalRecord.Models
+{
+    public class Patient
+    {
+        public int PatientID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string UCIN { get; set; } //jmbg - 	Unique Citizens Identity Number
+        public string HealthInsuranceNumber { get; set; } //broj zdravstvenog osiguranja
+        public string Sex { get; set; }
+    }
+}
