cf9a9c27522e3120566c3d3985757623d223dbd8
nemanjaletic <nemanja.letic@gmail.com>
2019-07-17 15:59:31 +0200

    Added Login page without validation

---
+++ ElectronicMedicalRecord/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using ElectronicMedicalRecord.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectronicMedicalRecord.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [HttpGet]
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Verify(Employed employed)
+        {
+            try
+            {
+
+                using (var db = new ElectronicMedicalRecord_DbContext())
+                {
+                    var query = (from tbl_employed in db.Employeds
+                                 where employed.Username == tbl_employed.Username && employed.Password == tbl_employed.Password
+                                 select new
+                                 {
+                                     tbl_employed.Username,
+                                     tbl_employed.Password
+                                 }).ToList();
+                    
+                }
+                
+                return View("IndexPage");
+            }
+            catch(Exception)
+            {
+                return View("LogInError");
+            }
+        }
+    }
+}
